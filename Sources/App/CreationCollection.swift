838332c25838312a108e93d92e50b4d20726e2bf
Hakon Hanesand <hakon@abello.no>
2016-10-25 15:31:26 -0700

    a

--- Sources/App/CreationCollection.swift
+++ Sources/App/CreationCollection.swift
@@ -79,7 +79,7 @@ final class CreationCollection : RouteCollection, EmptyInitializable {
         let upload = builder.grouped("upload")
         
         upload.post("image", Box.self) { request, box in
-            print(request)
+            Droplet.instance?.console.info(request.description, newLine: true)
 
             guard let fileData = request.multipart?["image"]?.file?.data else {
                 throw Abort.custom(status: .badRequest, message: "No file in request")
