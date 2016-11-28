d0746b083f23dc37d6869bf1ac3f388729ad03a9
Hakon Hanesand <hakon@abello.no>
2016-11-28 14:11:59 -0600

    update naming for service file

--- service_setup.sh
+++ service_setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+serviceFileName="instacrated.service.txt"
 serviceName="instacrated.service"
 servicePath="/etc/systemd/system/"
 
-cp "$serviceName" "$servicePath$serviceName"
-chmod 664 "$servicePath$serviceName"
+sudo cp "$serviceFileName" "$servicePath$serviceName"
+sudo chmod 664 "$servicePath$serviceName"
+
+systemctl daemon-reload
+systemctl start "$serviceName"
