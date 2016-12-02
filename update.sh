218a270542f3928cfeca8cd8724b246ed4b4cbff
Hakon Hanesand <hakon@subber.me>
2016-12-02 01:40:52 +0000

    add images

---
+++ update.sh
@@ -0,0 +1,5 @@
+git pull origin master
+vapor build
+vapor run --env=development
+terminal-notifier -title "Instacrate" -sound default -message "Done building" -actions "Ok"
+terminal-notifier -title "Instacrate" -sound default -message "Done building" -actions "Ok"
