e0fdc5b6db800e5407cea175c4dbdbd7c8c00274
Hakon Hanesand <hakon@abello.no>
2016-12-06 10:51:48 -0800

    handle more than one file

--- Sources/App/Controllers/LogFileController.swift
+++ Sources/App/Controllers/LogFileController.swift
@@ -14,29 +14,42 @@ final class LogFileController: ResourceRepresentable {
     
     func create(_ request: Request) throws -> ResponseRepresentable {
         
-        guard let fileData = request.multipart?["log"]?.file?.data else {
-            throw Abort.custom(status: .badRequest, message: "No file in request")
+        guard let files = request.multipart?.allItems else {
+            throw Abort.custom(status: .badRequest, message: "No files in request")
         }
         
         guard let workPath = Droplet.instance?.workDir else {
             throw Abort.custom(status: .internalServerError, message: "Missing working directory")
         }
         
-        guard let description = request.query?["description"]?.string else {
+        guard let description = request.json?["description"]?.string else {
             throw Abort.custom(status: .badRequest, message: "No file description")
         }
         
-        let name = "\(UUID().uuidString)-\(description).txt"
-        let imageFolder = "Private/Logs"
-        let saveURL = URL(fileURLWithPath: workPath).appendingPathComponent(imageFolder, isDirectory: true).appendingPathComponent(name, isDirectory: false)
+        let folder = "Private/Logs/\(description)"
+        let saveFolder = URL(fileURLWithPath: workPath).appendingPathComponent(folder, isDirectory: true)
         
-        do {
-            let data = Data(bytes: fileData)
-            try data.write(to: saveURL)
-        } catch {
-            throw Abort.custom(status: .internalServerError, message: "Unable to write multipart form data to file. Underlying error \(error)")
+        for (name, multipart) in files {
+            do {
+                if case let .file(file) = multipart {
+                    guard let fileName = file.name else {
+                        throw Abort.custom(status: .badRequest, message: "Missing file name for \(name)")
+                    }
+                    
+                    guard fileName.hasSuffix(".txt") else {
+                        throw Abort.custom(status: .badRequest, message: "Missing file extension for \(name)")
+                    }
+                    
+                    let saveURL = saveFolder.appendingPathComponent(fileName, isDirectory: false)
+                    
+                    let data = Data(bytes: file.data)
+                    try data.write(to: saveURL)
+                }
+            } catch {
+                throw Abort.custom(status: .internalServerError, message: "Unable to write multipart form data to file. Underlying error \(error)")
+            }
         }
-        
+
         return Response(status: .created)
     }
     
