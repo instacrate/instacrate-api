6cb904e77464989f1b0affb1d69e1c7e9b965244
Hakon Hanesand <hakon@abello.no>
2017-01-15 12:14:41 -0600

    do this.

--- Sources/App/StripeWebhookHandlerCollection.swift
+++ Sources/App/StripeWebhookHandlerCollection.swift
@@ -43,12 +43,14 @@ class StripeWebhookCollection {
                 throw Abort.custom(status: .internalServerError, message: "Wrong or missing metadata \(metadata)")
             }
 
-            guard let order_id = node["data", "object", "id"]?.string else {
+            guard let invoice_id = node["data", "object", "id"]?.string else {
                 throw Abort.custom(status: .badRequest, message: "could not find order id")
             }
 
-            var order = Order(with: subscription.id, vendor_id: vendor.id, box_id: box.id, shipping_id: shipping.id, customer_id: customer.id, order_id: order_id)
+            var order = Order(with: subscription.id, vendor_id: vendor.id, box_id: box.id, shipping_id: shipping.id, customer_id: customer.id, order_id: invoice_id)
             try order.save()
+
+            let _ = try Stripe.shared.updateInvoiceMetadata(for: order.id!.int!, invoice_id: invoice_id)
 
             return try Response(status: .ok, json: Node(node: ["status" : "ok"]).makeJSON())
         }
