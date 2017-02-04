5ba0721c4447fe804454cdfc9100f51fbe8b5488
Hakon Hanesand <hakon@abello.no>
2017-02-04 17:43:53 -0600

    Add Sentry and KSCrash, remove other dependencies.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "subber-api",
+    name: "instacrate-api",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/instacrate/Stripe", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/instacrate/Stripe", majorVersion: 1),
         .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver-Vapor.git", majorVersion: 1),
-        .Package(url: "https://github.com/nodes-vapor/bugsnag", majorVersion: 0),
-        .Package(url: "https://github.com/nodes-vapor/sanitized.git", majorVersion: 0)
+        .Package(url: "https://github.com/nodes-vapor/bugsnag", majorVersion: 1),
+        .Package(url: "https://github.com/nodes-vapor/sanitized.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
