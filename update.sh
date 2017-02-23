c78ec9b81f055c2c7f027a8bba2f82f7511d0f84
Hakon Hanesand <hakon@abello.no>
2017-02-22 20:39:46 -0600

    2

--- update.sh
+++ update.sh
@@ -2,10 +2,9 @@
 
 reset_development_server() {
 
-	devServiceFileName="dev-instacrated.service.txt"
-	devServiceName="dev-instacrated.service"
-
-	destinationPath="/etc/systemd/system/"
+	local devServiceFileName="dev-instacrated.service.txt"
+	local devServiceName="dev-instacrated.service"
+	local destinationPath="/etc/systemd/system/"
 
 	echo "\n>>>> sudo cp $devServiceFileName $destinationPath$devServiceName"
 	sudo cp "$devServiceFileName" "$destinationPath$devServiceName"
@@ -22,8 +21,9 @@ reset_development_server() {
 
 reset_production_server() {
 
-	prodServiceFileName="instacrated.service.txt"
-	prodServiceName="instacrated.service"
+	local prodServiceFileName="instacrated.service.txt"
+	local prodServiceName="instacrated.service"
+	local destinationPath="/etc/systemd/system/"
 
 	echo "\n>>>> sudo cp $prodServiceFileName $destinationPath$prodServiceName"
 	sudo cp "$prodServiceFileName" "$destinationPath$prodServiceName"
@@ -48,7 +48,7 @@ if [[ $(git diff --name-only HEAD~1 HEAD 'update.sh') ]]; then
 fi
 
 if [[ $(git diff --name-only HEAD~	1 HEAD nginx/) ]]; then
-	echo "\nsudo cp -ru nginx/* /etc/nginx/"
+	echo "\n>>>> sudo cp -ru nginx/* /etc/nginx/"
 	sudo cp -ru nginx/* /etc/nginx/
 fi
 
