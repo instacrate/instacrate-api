f7be692e77fb341427edca37caf5bff45c612703
Hakon Hanesand <hakon@abello.no>
2017-03-12 19:15:57 -0500

    update script

--- update.sh
+++ update.sh
@@ -13,10 +13,10 @@ reset_development_server() {
 	sudo chmod 664 "$destinationPath$devServiceName"
 
 	echo "\n>>>> systemctl daemon-reload"
-	systemctl daemon-reload
+	sudo systemctl daemon-reload
 
 	echo "\n>>>> systemctl restart $devServiceName"
-	systemctl restart "$devServiceName"
+	sudo systemctl restart "$devServiceName"
 }
 
 reset_production_server() {
@@ -32,14 +32,14 @@ reset_production_server() {
 	sudo chmod 664 "$destinationPath$devServiceName"
 
 	echo "\n>>>> systemctl daemon-reload"
-	systemctl daemon-reload
+	sudo systemctl daemon-reload
 
 	echo "\n>>>> systemctl restart $prodServiceName"
-	systemctl restart "$prodServiceName"
+	sudo systemctl restart "$prodServiceName"
 }
 
-echo "\n>>>> git pull origin master"
-git pull origin master
+echo "\n>>>> git pull origin"
+git pull origin
 
 if [ "$(git diff --name-only HEAD~ HEAD -- nginx/)" ]; then
 	echo "\n>>>> sudo cp -ru nginx/* /etc/nginx/"
@@ -48,12 +48,6 @@ fi
 
 echo "\n>>>> vapor build --release=true --fetch=false"
 vapor build --release=true --fetch=false --verbose
-
-echo "\n>>>> sudo systemctl restart instacrated.service"
-sudo systemctl restart instacrated.service
-
-echo "\n>>>> sudo systemctl restart dev-instacrated.service"
-sudo systemctl restart dev-instacrated.service
 
 if [ "$(git diff --name-only HEAD~1 HEAD -- instacrated.service.txt)" ]; then
     echo "\n>>>> Detected changes in production server configuration files!"
@@ -64,3 +58,9 @@ if [ "$(git diff --name-only HEAD~1 HEAD -- dev-instacrated.service.txt)" ]; the
 	echo "\n>>>> Detected changes in development server configuration files!"
 	reset_development_server
 fi
+
+echo "\n>>>> sudo systemctl restart instacrated.service"
+sudo systemctl restart instacrated.service
+
+echo "\n>>>> sudo systemctl restart dev-instacrated.service"
+sudo systemctl restart dev-instacrated.service
